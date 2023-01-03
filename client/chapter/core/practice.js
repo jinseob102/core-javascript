c06b881c99afa4d6fb43fa17c9a8a535e16e5ffc
jinseob102 <jinseob102@naver.com>
2023-01-03 23:56:58 +0900

    [추가] 함수 예제 연습

--- client/chapter/core/practice.js
+++ client/chapter/core/practice.js
@@ -1,2 +1,50 @@
+// //& 함수 - 첫번째 문제
+// function check(age) {
+//   if (age > 18) {
+//     return true;
+//   }else {
+//     return confirm('보호자 동의?');
+//   }
+// }
+// //& 함수 - 두번째 문제
+// function check(age) {
+//     return (age > 18) ? true : confirm('보호자 동의?');
+// //* return (age > 18) || confirm('보호자 동의?);
+// }
+// //& 함수 - 세번째 문제
+// function min(a, b){
+//   if(a > b){
+//     return b;
+//   }else {
+//     return a;
+//   }
+// }
+// function min(a, b) {
+//   return a > b ? a : b;
+// }
+// console.log(min(2, 5));
+// console.log(min(3, -1));
+// console.log(min(1, 1));
+// //& 함수 - 네번째 문제
+// function pow (x, n) {
+//   let result = x;
 
+//   for(let i = 1; i < n; i++) {
+//     result *= x;
+//   }
 
+//   return result;
+// }
+
+// alert(pow(10, 100));
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "동의하십니까?",
+  function() { alert("동의하셨습니다."); },
+  function() { alert("취소 버튼을 누르셨습니다.");  }  
+)
