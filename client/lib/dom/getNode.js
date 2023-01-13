4f43902d68e04243472149528fb373fc9843e8da
jinseob102 <jinseob102@naver.com>
2023-01-13 17:29:23 +0900

    [35일차] 계산기 - 주석변경_2

--- client/lib/dom/getNode.js
+++ client/lib/dom/getNode.js
@@ -1,7 +1,7 @@
 
 
 //* getNode를 쓰는 이유는?
-//* 
+//* 문자 타입인 것을 확인 하고, CSS선택자로 바꿔주기 위해서!
 function getNode(node){
   if(typeof node !== 'string'){
     throw new Error('getNode 함수의 인자는 문자 타입 이여야 합니다.');
@@ -13,7 +13,7 @@ function getNode(node){
 }
 
 //* getNodes를 쓰는 이유는?
-//*
+//* 문자 타입인 것을 확인 하고, CSS선택자로 바꿔주기 위해서!
 function getNodes(node){
   if(typeof node !== 'string'){
     throw new Error('getNode 함수의 인자는 문자 타입 이여야 합니다.');
