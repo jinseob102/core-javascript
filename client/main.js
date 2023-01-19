5ae22fa3b7056a657871087fb1194c780f78ba19
jinseob102 <jinseob102@naver.com>
2023-01-19 16:10:44 +0900

    [39일차] 스토리지 - 마지막 자스 수업

--- client/main.js
+++ client/main.js
@@ -1,6 +1,33 @@
 
-console.log('hello~')
-console.log(window)
+
+
+import { getNode as $, loadStorage, saveStorage } from "./lib/index.js";
 
 
 
+
+
+
+
+
+const textField = $('#textField')
+const deleteButton = $('input[value="삭제"]');
+
+
+
+loadStorage('area').then((res)=>{
+  textField.value = res;
+})
+
+
+function inputHandler(){
+    SVGAElement.Storage('area')
+}
+
+
+
+
+
+
+//^ 인풋 이벤트는 값이 추가되는 걸 감지, 새로고침하면 날아감
+textField.addEventListener('input',inputHandler)
